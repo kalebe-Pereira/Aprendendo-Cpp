ab7ff6fc58b796ec4ece5b27aab444a7027ba393
Kalebe Pereira <kalebepereira73@gmai.com>
2026-03-19 17:40:21 -0300

    Arquivo para declaração múltipla

---
+++ declaracaoMultiplaDeVariavel.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+
+int main()
+{
+    // A declaração múltipla de variável é o ato de declarar diversas variáveis ao mesmo tempo, facilitando a leitura e diminuindo a quantidade de linhas. Exemplo:
+
+    int a, b, c;
+
+    // A declaração pode ser feito incrementando valores neles. Exemplo:
+    int d = 1, e = 2, f = 3;
+
+    // Outro método de declarar as varíaveis seria orientando os tipos. Exemplo:
+
+    int g = 9; float h = 8.8; bool i = true; // Nestes casos, utiliza-se o ponto e vírgula.
+
+    std::cout << g << '\n' << h << '\n' << i << '\n'; // Demonstração
+
+    return 0;
+}
---
+++ variaveis.cpp
@@ -0,0 +1,37 @@
+#include "iostream"
+
+int main()
+{
+    // Em C++ temos os seguintes tipos de variáveis:
+
+    /*
+    Int -> Inteiro.
+    Char -> Caracteres.
+    Float -> Tipo fracionado (Real).
+    double -> Tipo fracionado com mais casas decimais.
+    bool -> Boleano.
+    String -> Cadeia de caracteres.
+    */
+
+    // Sendo declaradas desse modo:
+    // <tipo> <nome_da_variavel>
+
+    int numero;
+    double valor_fracionado;
+    char caracteres;
+    bool boleano;
+    float reais;
+    std::string cadeiaDeCaracteres;
+    
+    // Pode-se criar uma variável e já inici-la com algum valor. Exemplo:
+    // PS.: Após a criação de uma variáveis, não é necessário utilizar o tipo dele
+
+    numero = 123;
+    valor_fracionado = 3.1415;
+    caracteres = 'A';
+    boleano = true;
+    reais = 2.4;
+    cadeiaDeCaracteres = "Insira o texto aqui: ";
+    
+    return 0;
+}
