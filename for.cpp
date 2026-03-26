368ed38fad47bd6c594c2a8200ff91e489e51151
Kalebe Pereira <kalebepereira73@gmai.com>
2026-03-26 16:04:29 -0300

    Adicionando o arquivo for.cpp

---
+++ for.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+
+int main()
+{
+    // O loop For é uma estrutura de repetição pré-definidos, efetuando as instruções e incrementando a variável contadora automaticamente. O for pode ser definido como "Para algo, faça isso".
+
+    // Exemplo de Utilização:
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "Numero: " << i << '\n';
+    }
+
+    // No for, da para declarar e incrementar a variável dentro do método
+
+    
+    return 0;
+}
